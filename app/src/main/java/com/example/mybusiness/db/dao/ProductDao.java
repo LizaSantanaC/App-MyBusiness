89497a71b685de898fdb50805005f54a8df4ba1c
LizaSantanaC <santanaliza.c@gmail.com>
2020-12-13 10:29:52 -0600

    SE CREÓ EL PAQUETE DAO

---
+++ app/src/main/java/com/example/mybusiness/db/dao/ProductDao.java
@@ -0,0 +1,38 @@
+package com.example.mybusiness.db.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import com.my.business.db.entity.Product;
+
+import java.util.List;
+
+@Dao
+public interface ProductDao {
+
+    @Insert
+    void insert(Product product);
+
+    @Update(entity = Product.class)
+    void update(Product...product);
+
+
+    @Query("SELECT * FROM productos")
+    LiveData<List<Product>> getAll();
+
+    @Transaction
+    @Query("SELECT * FROM productos WHERE nombre LIKE :name")
+    List<Product> findByName(String name);
+
+    @Transaction
+    @Query("SELECT * FROM productos WHERE codigo == :code")
+    LiveData<Product> findByCode(String code);
+
+    @Delete
+    int delete(Product product);
+}
