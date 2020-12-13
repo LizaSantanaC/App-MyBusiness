621e3be779449ecc57337998f02caf4f4ad39f98
LizaSantanaC <santanaliza.c@gmail.com>
2020-12-13 12:47:12 -0600

    SE CREÓ EL PAQUETE DE REPOSITORY - CLASE REPOSITORY

---
+++ app/src/main/java/com/example/mybusiness/db/repository/ProductRepository.java
@@ -0,0 +1,54 @@
+package com.example.mybusiness.db.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import com.example.mybusiness.db.dao.ProductDao;
+import com.example.mybusiness.db.entity.Product;
+import com.example.mybusiness.db.roo
+
+import com.my.business.db.dao.ProductDao;
+import com.my.business.db.entity.Product;
+import com.my.business.db.room.MyBusinessDB;
+
+import java.util.List;
+
+public class ProductRepository {
+    private ProductDao mProductDao;
+    private LiveData<List<Product>> mAllProducts;
+
+    public ProductRepository(Application application){
+        MyBusinessDB db = MyBusinessDB.getDatabase(application);
+        mProductDao = db.productDao();
+        mAllProducts = mProductDao.getAll();
+    }
+
+    public LiveData<List<Product>> getAllProducts() {
+        return mAllProducts;
+    }
+
+    public void insert(Product product) {
+        MyBusinessDB.databaseWriteExecutor.execute(() -> {
+            mProductDao.insert(product);
+        });
+    }
+
+    public void update(Product product) {
+        MyBusinessDB.databaseWriteExecutor.execute(() -> {
+            mProductDao.update(
+                    product
+            );
+        });
+    }
+
+    public void delete(Product product) {
+        MyBusinessDB.databaseWriteExecutor.execute(() -> {
+            mProductDao.delete(product);
+        });
+    }
+
+
+    public LiveData<Product> findByCode(String code) {
+        return mProductDao.findByCode(code);
+    }
+}
